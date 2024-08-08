c4e2ae2ebaa3bb1d70e65120ddcd1d885b5b34bb
brunoLloret <163921771+brunoLloret@users.noreply.github.com>
2024-08-07 22:46:01 -0400

    Garden and Limbo with sprite semi functioning

--- ClubPenguinClone/FE/components/common/Character.tsx
+++ ClubPenguinClone/FE/components/common/Character.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import CharacterMarker from "./CharacterMarker";
 
 interface CharacterProps {
   x: number;
@@ -8,6 +9,12 @@ interface CharacterProps {
   body: string;
   head: string;
   weapon: string;
+  onMarkerClick: (position: Position) => void; // Add prop for click handler
+}
+
+interface Position {
+  x: number;
+  y: number;
 }
 
 const Character: React.FC<CharacterProps> = ({
@@ -18,8 +25,12 @@ const Character: React.FC<CharacterProps> = ({
   body,
   head,
   weapon,
+
+  onMarkerClick,
 }) => {
   const spriteSize = 128;
+
+  const position: Position = { x, y };
 
   const getSpritePosition = (index: number) => {
     const row = Math.floor(index / 8);
@@ -28,45 +39,48 @@ const Character: React.FC<CharacterProps> = ({
   };
 
   return (
-    <div
-      style={{
-        position: "absolute",
-        left: `${x}px`,
-        top: `${y}px`,
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <div
-        style={{
-          width: `${spriteSize}px`,
-          height: `${spriteSize}px`,
-          backgroundImage: `url(${body})`,
-          backgroundPosition: getSpritePosition(direction * 8 + frame),
-        }}
-      />
+    <>
       <div
         style={{
           position: "absolute",
-          top: 0,
-          left: 0,
-          width: `${spriteSize}px`,
-          height: `${spriteSize}px`,
-          backgroundImage: `url(${head})`,
-          backgroundPosition: getSpritePosition(direction * 8 + frame),
+          left: `${x}px`,
+          top: `${y}px`,
+          transform: "translate(-50%, -50%)",
         }}
-      />
-      <div
-        style={{
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: `${spriteSize}px`,
-          height: `${spriteSize}px`,
-          backgroundImage: `url(${weapon})`,
-          backgroundPosition: getSpritePosition(direction * 8 + frame),
-        }}
-      />
-    </div>
+      >
+        <div
+          style={{
+            width: `${spriteSize}px`,
+            height: `${spriteSize}px`,
+            backgroundImage: `url(${body})`,
+            backgroundPosition: getSpritePosition(direction * 8 + frame),
+          }}
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: `${spriteSize}px`,
+            height: `${spriteSize}px`,
+            backgroundImage: `url(${head})`,
+            backgroundPosition: getSpritePosition(direction * 8 + frame),
+          }}
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: `${spriteSize}px`,
+            height: `${spriteSize}px`,
+            backgroundImage: `url(${weapon})`,
+            backgroundPosition: getSpritePosition(direction * 8 + frame),
+          }}
+        />
+      </div>
+      <CharacterMarker position={position} onClick={onMarkerClick} />
+    </>
   );
 };
 
